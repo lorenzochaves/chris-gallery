1957c4106d4d6637b005c44ba9c330b8805faafd
jdaniielc <jdsc2@cin.ufpe.br>
2025-05-13 16:55:51 -0300

    fix: update CORS settings to include Set-Cookie headers for improved cookie handling

--- server/server.js
+++ server/server.js
@@ -19,7 +19,8 @@ app.use(
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "Set-Cookie"],
+    exposedHeaders: ["Set-Cookie"],
   })
 );
 
